1b29138eeaf7157cea44ea05f4db8bd05f10e469
Mogan Xumerle <moganrz@gmail.com>
2018-07-17 23:24:15 -0300

    Trabalhando com Polimorfismo em Python

--- modelo.py
+++ modelo.py
@@ -20,11 +20,17 @@ class Programa:
     def nome(self, nome):
         self._nome = nome.title()
 
+    def __str__(self):
+        return f'Nome: {self._nome} - Likes: {self._likes}'
+
 class Filme(Programa):
 
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+
+    def __str__(self):
+        return f'Nome: {self._nome} - {self.duracao} min - Likes: {self._likes}'
 
 
 class Serie(Programa):
@@ -32,6 +38,9 @@ class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+
+    def __str__(self):
+        return f'Nome: {self._nome} - {self.temporadas} temporadas - Likes: {self._likes}'
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -42,5 +51,10 @@ vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+#print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
+#print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+
+listinha = [vingadores, atlanta]
+
+for programa in listinha:
+    print(programa)
