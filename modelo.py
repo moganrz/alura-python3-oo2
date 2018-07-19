37aa92f9131704855a41626723e12f60f58fc231
Mogan Xumerle <moganrz@gmail.com>
2018-07-18 22:14:49 -0300

    Heranca de classes Built-in no Python

--- modelo.py
+++ modelo.py
@@ -42,19 +42,40 @@ class Serie(Programa):
     def __str__(self):
         return f'Nome: {self._nome} - {self.temporadas} temporadas - Likes: {self._likes}'
 
+
+class Playlist:
+
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
-vingadores.dar_likes()
-vingadores.dar_likes()
-vingadores.dar_likes()
+tmep = Filme('todo mundo em panico', 1999, 100)
+demolidor = Serie('demolidor', 2016, 2)
 
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
 
-#print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-#print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+listinha = [atlanta, vingadores, demolidor, tmep]
+minha_playlist = Playlist('fim de semana', listinha)
 
-listinha = [vingadores, atlanta]
-
-for programa in listinha:
+for programa in minha_playlist.listagem:
     print(programa)
+
+print(f'Tamanho: {len(minha_playlist.listagem)}')
